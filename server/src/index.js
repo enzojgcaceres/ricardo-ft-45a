282559ceecec9cc05500251c4470d47a0a996936
enzojgcaceres <enzojgcaceres@gmail.com>
2023-12-14 19:13:59 -0300

    Multiples cambios previos al M4

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,12 @@
 const server = require("./app");
 const PORT = 3001;
+const { conn } = require("./DB_connection")
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+//* Sincronizar la instancia "sequelize" al servidor
+conn.sync({ force: true }) //* Retorna una promesa
+.then(()=>{
+   server.listen(PORT, () => {
+      console.log('Server raised in port: ' + PORT);
+   });
+})
+.catch(error => console.log(error.message))
